a9da3b80e653e121599085368b4690fdbd5cf86d
Gabriel Davila <ferdavi6@gmail.com>
2018-03-26 14:28:06 -0300

    update

--- Script_iptvMulticast.py
+++ Script_iptvMulticast.py
@@ -12,9 +12,9 @@ from snmp import SnmpError
 
 
 # definitions 
-myIP= '10.32.173.143'
+myIP= '10.254.1.33'
 interval = 10 #seconds
-duration = 120 #seconds
+duration = 1200 #seconds
 
 
 # create CM
@@ -40,7 +40,7 @@ start = time.time()
 
 Ccer = {}
 Cer = {}
-
+print("timeStamp,", "MAC,", "FREQ [MHz], ", "SNR [dB], ", "CCER [%], ", "CER [%], ", "Bandwidth [Mbps]")
 while (time.time()-start < duration):
     time.sleep(interval)
     timeStamp = time.time() -start
@@ -50,8 +50,12 @@ while (time.time()-start < duration):
     ChSnr = CmDocIf.updateDownSnr()
     bandwidth_Mbps = round(((int(OctetsIn_f) - int(OctetsIn_i)) /(timeStamp_f-timeStamp_i))*8/1E6,2)
     for ch in Snr.keys():
-        Ccer[ch] = round((int(CorrC_f[ch])-int(CorrC_i[ch])) / (int(CorrC_f[ch])-int(CorrC_i[ch]) +int(UncorrC_f[ch])-int(UncorrC_i[ch]) + int(UnerrC_f[ch])- int(UnerrC_i[ch])), 2)
-        Cer[ch] =  round((int(UncorrC_f[ch]) -int(UncorrC_i[ch])) / (int(CorrC_f[ch])-int(CorrC_i[ch]) +int(UncorrC_f[ch])-int(UncorrC_i[ch]) + int(UnerrC_f[ch])-int(UnerrC_i[ch])), 2)
+        try:
+            Ccer[ch] = round((int(CorrC_f[ch])-int(CorrC_i[ch])) / (int(CorrC_f[ch])-int(CorrC_i[ch]) +int(UncorrC_f[ch])-int(UncorrC_i[ch]) + int(UnerrC_f[ch])- int(UnerrC_i[ch])), 2)
+            Cer[ch] =  round((int(UncorrC_f[ch]) -int(UncorrC_i[ch])) / (int(CorrC_f[ch])-int(CorrC_i[ch]) +int(UncorrC_f[ch])-int(UncorrC_i[ch]) + int(UnerrC_f[ch])-int(UnerrC_i[ch])), 2)
+        except ZeroDivisionError :
+            Ccer[ch] = "PS"
+            Cer[ch] = "PS"
         print(timeStamp, ",", CmMac[2:], ",",round(int(Freq[ch])/1E6,2), ",", round(int(Snr[ch])/10,2), ",", Ccer[ch], ",",Cer[ch],"," ,bandwidth_Mbps)
     CorrC_i, UncorrC_i, UnerrC_i  = CorrC_f, UncorrC_f, UnerrC_f
     OctetsIn_i=OctetsIn_f
